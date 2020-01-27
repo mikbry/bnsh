ffe69ac3005741637f987db1ce07faf777969315
Mik <mik@opla.ai>
2020-01-27 10:21:08 +0100

    chore: create project files

---
+++ src/cli.js
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) Mik BRY
+ * mik@mikbry.com
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import chalk from 'chalk';
+import emoji from 'node-emoji';
+import minimist from 'minimist';
+import Bnsh from './Bnsh';
+
+class BnshCli {
+  constructor(args, output, exit) {
+    this.output = output;
+    this.args = args;
+    this.exit = exit;
+    this.chalk = chalk;
+    this.options = {
+      string: ['src'],
+      boolean: ['help'],
+      stopEarly: true,
+      alias: {
+        s: 'src',
+        h: 'help',
+      },
+      default: {
+        src: './benchmark',
+        help: false,
+      },
+    };
+  }
+
+  log(text, ...opts) {
+    this.output.log(text, ...opts);
+  }
+
+  error(text, ...opts) {
+    this.output.error(text, ...opts);
+  }
+
+  header() {
+    // const { version } = getPackage('bnsh');
+    const version = 'WIP';
+    this.log(chalk.bold(emoji.emojify(`Bnsh cleans package before publishing v${version}`)));
+  }
+
+  usage() {
+    this.log('Usage: $ bnsh [options]');
+  }
+
+  help() {
+    this.usage();
+    this.log('');
+    this.log('Displays help informations.');
+    this.log('');
+    this.log('Options:');
+    this.log('Examples:');
+    this.log('$ bnsh');
+  }
+
+  async execute() {
+    this.hrstart = process.hrtime();
+    this.header();
+    const args = minimist(this.args, this.options);
+    if (args.help) {
+      // Display help
+      this.help();
+    } else {
+      // WIP execute
+      const bnsh = new Bnsh(args.dist, args.n, args._);
+      // const pkg = getPackage();
+      // await bnsh.transform(pkg);
+      await bnsh.write();
+      // this.log('args=', args);
+    }
+  }
+}
+/* istanbul ignore next */
+const start = async (output = console) => {
+  const version = process.versions.node;
+  const major = parseInt(version.split('.')[0], 10);
+
+  if (major < 10) {
+    output.error(`Node version ${version} is not supported, please use Node.js 10.0 or higher.`);
+    process.exit(1);
+  }
+
+  // Grab arguments
+  const [, , ...args] = process.argv;
+  const cli = new BnshCli(args, output, process.exit);
+  await cli.execute();
+  return cli;
+};
+export { BnshCli };
+export default start;
