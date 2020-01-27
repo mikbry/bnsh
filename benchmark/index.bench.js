a68cc99e10faf4b1100a1cf8ce13fa68975a2d41
Mik <mik@opla.ai>
2020-01-27 19:09:37 +0100

    feat: add basecode

--- benchmark/index.bench.js
+++ benchmark/index.bench.js
@@ -7,3 +7,12 @@
  */
 
 // TODO
+console.log('index.bench.js : [TODO] import');
+
+suite('Finding a substring', () => {
+  benchmark('RegExp#test', () => /o/.test('Hello World!'));
+
+  benchmark('String#indexOf', () => 'Hello World!'.indexOf('o') > -1);
+
+  benchmark('String#match', () => !!'Hello World!'.match(/o/));
+});
